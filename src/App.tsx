ddd26a3bcc4930aa5a1a0052acd26557cf6e6bc5
RamonSalem <salemramon@gmail.com>
2021-09-16 12:01:01 -0300

    initial config

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        This is a base
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
